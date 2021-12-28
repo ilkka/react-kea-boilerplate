71aaa644acdafd80bc6b063ecd6dea060a07ea51
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2021-12-28 11:57:30 +0200

    Probably will use the new JSX transform

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
